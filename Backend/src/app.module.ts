8e772acbe6f9efcf997cea947df28eb5fed76884
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-25 09:05:01 +0700

    feat:" Uset variable env into main and module app such as change PORT and config connect DB":

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+     isGlobal:true
+  })
+],
   controllers: [],
   providers: [], 
 })
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  await app.listen(3000);
+  const configService=app.get(ConfigService);
+  const PORT=configService.get<number>('PORT');
+  await app.listen(PORT);
 }
 bootstrap();
