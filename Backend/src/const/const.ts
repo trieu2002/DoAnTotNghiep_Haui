6ec72d21936a7abdb36af05aea3942e9651c78a0
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-25 09:41:26 +0700

    feat:"Add variable const route"

---
+++ Backend/src/const/const.ts
@@ -0,0 +1,8 @@
+export const AUTH_ROUTE:string='auth';
+export const USER_ROUTE:string='users'
+export const COMPANIES_ROUTE:string="companies";
+export const RESUMES_ROUTE:string="resumes";
+export const JOBS_ROUTE:string='jobs';
+export const ROLES_ROUTE:string='roles';
+export const PERMISSIONS_ROUTE:string='permissions';
+export const SUBSCRIBERS_ROUTE:string='subcribers';
