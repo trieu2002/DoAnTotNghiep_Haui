dbfb7f9fe361ea22e0262eaf5823dae0936e96a6
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-25 09:33:06 +0700

    fix:"Create schema example user"

--- Backend/src/users/schema/user.schema.ts
+++ Backend/src/users/schema/user.schema.ts
@@ -1,1 +1,32 @@
-export class User {}
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+/**
+ * Link documenet create schema:https://docs.nestjs.com/techniques/mongodb
+ */
+@Schema({
+    timestamps:true,
+    versionKey:false
+})
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+  @Prop()
+  phone:string
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  age:number;
+
+  @Prop()
+  address:string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
