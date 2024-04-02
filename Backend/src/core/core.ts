b158b5cdfffb5eb5cc20394c18db3e796cf3d2c2
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-04-02 09:00:28 +0700

    feat:"Add customize response data to client"

--- Backend/src/core/core.ts
+++ Backend/src/core/core.ts
@@ -1,4 +1,15 @@
 import { SetMetadata } from '@nestjs/common';
-
 export const IS_PUBLIC_KEY:string = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const DUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
+export const RESPONSE_MESSAGE:string='Response_Message';
+export const ResponseMessage=(message:string)=>{
+     SetMetadata(RESPONSE_MESSAGE,message);
+}
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,14 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
+
   app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
-  
+  app.useGlobalInterceptors(new TransformInterceptor(app.get(Reflector)));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
